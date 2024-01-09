a063988b8847a330a63ec645e2ac9ebaac14f5c0
Yevhen Sidelnyk <zsidelnik@gmail.com>
2024-01-09 20:19:20 +0200

    add users seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Entities\Customer\Customer;
+use App\Entities\Freelancer\Freelancer;
+use App\ValueObjects\Email;
+use App\ValueObjects\Money;
+use App\ValueObjects\Password;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +16,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        /** @var \Doctrine\ORM\EntityManagerInterface $entityManager */
+        $entityManager = app()->get(\Doctrine\ORM\EntityManagerInterface::class);
+
+        $customers = [
+            [
+                'customer1@test.com',
+                'customer1@test.com',
+            ],
+            [
+                'customer2@test.com',
+                'customer2@test.com',
+            ],
+        ];
+
+        foreach ($customers as [$email, $pass]) {
+            $customer = Customer::register(
+                Email::from($email),
+                Password::fromRaw($pass),
+            );
+
+            $entityManager->persist($customer);
+        }
+
+        $entityManager->flush();
+
+        $freelancers = [
+            [
+                'freelancer1@test.com',
+                'freelancer1@test.com',
+                10,
+            ],
+            [
+                'freelancer2@test.com',
+                'freelancer2@test.com',
+                20,
+            ],
+        ];
+
+        foreach ($freelancers as [$email, $pass, $rate]) {
+            $freelancer = Freelancer::register(
+                Email::from($email),
+                Password::fromRaw($pass),
+                Money::usd($rate),
+            );
+
+            $entityManager->persist($freelancer);
+        }
+
+        $entityManager->flush();
     }
 }
